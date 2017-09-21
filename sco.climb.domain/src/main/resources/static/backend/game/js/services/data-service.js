77503949f044b80843144e6ac2a55fd41f826b6d
micnori <micnori@yahoo.com>
2017-09-21 14:29:47 +0200

    fix Authorization header

--- sco.climb.domain/src/main/resources/static/backend/game/js/services/data-service.js
+++ sco.climb.domain/src/main/resources/static/backend/game/js/services/data-service.js
@@ -42,7 +42,7 @@ function ($q, $http, $rootScope, $timeout) {
               } else if(type === 'classes') {
               	fetchUrl = baseUrl + "/api/game/" + ownerId + "/" + instituteId + "/" + schoolId + "/classes";
               }
-              return $http.get(fetchUrl, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              return $http.get(fetchUrl, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
 
               // PER IL TESTING IN LOCALE CON IL LOCAL STORAGE
               /*
@@ -60,23 +60,23 @@ function ($q, $http, $rootScope, $timeout) {
                 sendUrl = baseUrl + "/api/game/" + element.ownerId + "/" + element.pedibusGameId + "/itinerary/" + element.objectId;
               } else if(type === 'legs') {
               	sendUrl = baseUrl + "/api/game/" + element.ownerId + "/" + element.pedibusGameId + "/itinerary/" + element.objectId + "/" + type;
-                return $http.put(sendUrl, element.legs, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+                return $http.put(sendUrl, element.legs, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
               } else if(type == 'school') {
               	sendUrl = baseUrl + "/api/school/" + element.ownerId + "/" + element.objectId;
               } else if(type == 'stops') {
               	sendUrl = baseUrl + "/api/stop/" + element.ownerId + "/" + element.routeId;              	
-             	 	return $http.post(sendUrl, element.stops, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+             	 	return $http.post(sendUrl, element.stops, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
               } else if(type == 'route') {
               	sendUrl = baseUrl + "/api/route/" + element.ownerId + "/" + element.objectId;
               } else if(type == 'child') {
               	sendUrl = baseUrl + "/api/child/" + element.ownerId + "/" + element.objectId;
               }
-              return $http.put(sendUrl, element, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              return $http.put(sendUrl, element, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
           },
           getInstitutesList: function(owner)
           {
               var urlInstituteList = baseUrl + "/api/institute/" + owner;
-              return $http.get(urlInstituteList, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              return $http.get(urlInstituteList, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
           },
           saveData: function(type, element)           
           {
@@ -89,16 +89,16 @@ function ($q, $http, $rootScope, $timeout) {
                 postUrl = baseUrl + "/api/game/" + element.ownerId + "/" + element.pedibusGameId + "/itinerary";
               } else if(type === 'legs') {
                 postUrl = baseUrl + "/api/game/" + element.ownerId + "/" + element.pedibusGameId + "/itinerary/" + element.objectId + "/" + type;
-                return $http.post(postUrl, element.legs, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+                return $http.post(postUrl, element.legs, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
               } else if(type == 'stops') {
               	postUrl = baseUrl + "/api/stop/" + element.ownerId + "/" + element.routeId;              	
-              	return $http.post(postUrl, element.stops, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              	return $http.post(postUrl, element.stops, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
               } else if(type == 'route') {
               	postUrl = baseUrl + "/api/route/" + element.ownerId;
               } else if(type == 'child') {
               	postUrl = baseUrl + "/api/child/" + element.ownerId;
               }
-              return $http.post(postUrl, element, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              return $http.post(postUrl, element, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
 
               // localStorage.setItem(type + '_' + localId,
 							// angular.toJson(data)); // PER IL TESTING IN LOCALE CON IL
@@ -114,7 +114,7 @@ function ($q, $http, $rootScope, $timeout) {
               } else {
               	deleteUrl = baseUrl + "/api/" + type + "/" + element.ownerId + "/" + element.objectId;
               }
-              return $http.delete(deleteUrl, {headers: {'Autorization': 'Bearer ' + $rootScope.profile.token}});
+              return $http.delete(deleteUrl, {headers: {'Authorization': 'Bearer ' + $rootScope.profile.token}});
           },
           logout: logout
       };
