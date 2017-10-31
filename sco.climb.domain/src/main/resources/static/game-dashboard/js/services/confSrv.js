974fe74d9c6f8de41f8543375f8a454922c89fb3
micnori <micnori@yahoo.com>
2017-10-31 15:26:52 +0100

    fix conf

--- sco.climb.domain/src/main/resources/static/game-dashboard/js/services/confSrv.js
+++ sco.climb.domain/src/main/resources/static/game-dashboard/js/services/confSrv.js
@@ -3,11 +3,11 @@ angular.module('climbGame.services.conf', [])
   .factory('configService', function () {
     var configService = {}
     var DEVELOPMENT = true
-    //var URL = 'https://' + (DEVELOPMENT ? 'climbdev' : 'climb') + '.smartcommunitylab.it/v2'
-    var URL = 'http://192.168.42.60:6020/domain'
-    var FOOT_CONSTANT = 'piedi'
-    var BOAT_CONSTANT = 'nave'
-    var PLANE_CONSTANT = 'aereo'
+    var URL = 'https://' + (DEVELOPMENT ? 'climbdev' : 'climb') + '.smartcommunitylab.it/v2'
+    //var URL = 'http://192.168.42.60:6020/domain'
+    var FOOT_CONSTANT = 'foot'
+    var BOAT_CONSTANT = 'boat'
+    var PLANE_CONSTANT = 'plane'
     var httpTimeout = 10000
     var DEFAULT_CENTER_MAP = [37.973378, 23.730957]
     var DEFAULT_ZOOM_MAP = 4
