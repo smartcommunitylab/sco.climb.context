b5072ee1630ccb83753a4527a0e45be946980e11
micnori <micnori@yahoo.com>
2017-11-08 15:56:56 +0100

    add CustomUserDetailsService

---
+++ sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/security/CustomUserDetailsService.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2015 Smart Community Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.smartcommunitylab.climb.domain.security;
+
+import it.smartcommunitylab.climb.contextstore.model.User;
+import it.smartcommunitylab.climb.domain.storage.RepositoryManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private RepositoryManager storage;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = storage.getUserByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException(String.format("user %s not found", username));
+		}
+		DataSetInfo dataSetInfo = new DataSetInfo();
+		dataSetInfo.setCf(user.getCf());
+		dataSetInfo.setEmail(user.getEmail());
+		dataSetInfo.setName(user.getName());
+		dataSetInfo.setSurname(user.getSurname());
+		dataSetInfo.setSubject(user.getSubject());
+		dataSetInfo.setRoles(user.getRoles());
+		dataSetInfo.setOwnerIds(user.getOwnerIds());
+		return new DataSetDetails(dataSetInfo);
+	}
+
+
+	
+}
