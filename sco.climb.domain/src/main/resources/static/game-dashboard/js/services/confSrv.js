c5c53b6c3023b83021eb2e49e29b32f022190e4a
micnori <micnori@yahoo.com>
2017-11-15 14:47:45 +0100

    conf for prod

--- sco.climb.domain/src/main/resources/static/game-dashboard/js/services/confSrv.js
+++ sco.climb.domain/src/main/resources/static/game-dashboard/js/services/confSrv.js
@@ -2,7 +2,7 @@
 angular.module('climbGame.services.conf', [])
   .factory('configService', function () {
     var configService = {}
-    configService.DEVELOPMENT = true
+    configService.DEVELOPMENT = false
     var URL = 'https://' + (configService.DEVELOPMENT ? 'climbdev' : 'climb') + '.smartcommunitylab.it/v2'
     
     configService.FOOT_CONSTANT = 'foot'
