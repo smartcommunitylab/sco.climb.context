7e71c90a78eadb209e3d17228d7c1b5135de571c
dema121 <andrea.dematte@gmail.com>
2017-12-21 13:51:30 +0100

    User console: search functionality

--- sco.climb.domain/src/main/resources/static/backend/user/js/app.js
+++ sco.climb.domain/src/main/resources/static/backend/user/js/app.js
@@ -127,3 +127,20 @@ angular.module('climbGameUser', [
     }
   }
 })
+
+.directive('inputClear', function() {
+  return {
+    restrict: 'A',
+    compile: function (element, attrs) {
+        var color = attrs.inputClear;
+        var style = color ? "color:" + color + ";" : "";
+        var action = attrs.ngModel + " = ''";
+        element.after(
+            '<md-button class="animate-show md-icon-button md-accent"' +
+            'ng-show="' + attrs.ngModel + '" ng-click="' + action + '"' +
+            'style="position: absolute; top: -16px; right: -6px; margin: 13px 0px;">' +
+            '<div style="' + style + '">x</div>' +
+            '</md-button>');
+    }
+  };
+})
