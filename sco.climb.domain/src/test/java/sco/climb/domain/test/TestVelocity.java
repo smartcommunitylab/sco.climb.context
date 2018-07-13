27b59593ecad1e0dfaf6d31301c6fcf24f6741d0
micnori <micnori@yahoo.com>
2018-07-13 16:16:08 +0200

    fix templates

--- sco.climb.domain/src/test/java/sco/climb/domain/test/TestVelocity.java
+++ sco.climb.domain/src/test/java/sco/climb/domain/test/TestVelocity.java
@@ -19,7 +19,7 @@ import it.smartcommunitylab.climb.domain.model.PedibusItineraryLeg;
 public class TestVelocity {
 
 	@Test
-	public void testVelocity() {
+	public void testVelocityConstants() {
 		VelocityEngine velocityEngine = new VelocityEngine();
 		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
 		velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
@@ -47,4 +47,65 @@ public class TestVelocity {
 		
 		System.out.println(writer.toString());
 	}
+	
+	@Test
+	public void testVelocityCalendarTrips() {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
+		velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+		velocityEngine.init();
+		    
+		Template t = velocityEngine.getTemplate("game-template/calendartrips.vm");
+		
+		Map<String, String> params = new HashMap<>();
+		params.put("const_pandr_distance", "100");
+		params.put("const_bus_distance", "100");
+		params.put("const_zeroimpact_distance", "100");
+		params.put("const_cloudy_bonus", "100");
+		params.put("const_rain_bonus", "100");
+		params.put("const_snow_bonus", "100");
+		
+		List<PedibusItineraryLeg> legs = new ArrayList<>();
+		
+		VelocityContext context = new VelocityContext();
+		context.put("params", params);
+		context.put("legList", legs);
+		context.put("Utils", Utils.class);
+		
+		StringWriter writer = new StringWriter();
+		t.merge(context, writer);
+		
+		System.out.println(writer.toString());
+	}
+	
+	@Test
+	public void testVelocityLegsBadges() {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
+		velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+		velocityEngine.init();
+		    
+		Template t = velocityEngine.getTemplate("game-template/legsbadges.vm");
+		
+		Map<String, String> params = new HashMap<>();
+		params.put("const_zi_solo_bonus", "1000.0");
+		params.put("final_destination", "Fine Viaggio");
+		
+		List<PedibusItineraryLeg> legs = new ArrayList<>();
+		PedibusItineraryLeg leg = new PedibusItineraryLeg();
+		leg.setName("Tappa 1");
+		leg.setScore(1000);
+		legs.add(leg);
+		
+		VelocityContext context = new VelocityContext();
+		context.put("params", params);
+		context.put("legList", legs);
+		context.put("Utils", Utils.class);
+		
+		StringWriter writer = new StringWriter();
+		t.merge(context, writer);
+		
+		System.out.println(writer.toString());
+	}
+	
 }
