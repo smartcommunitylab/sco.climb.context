525ebbf4a10a0b0cde6d499012cfe39e01c6e05c
micnori <micnori@yahoo.com>
2019-03-11 16:47:33 +0100

    better management of upload content

--- sco.climb.domain/src/main/resources/static/backend/game/js/controllers/paths/createNewMultimediaDialogCtrl.js
+++ sco.climb.domain/src/main/resources/static/backend/game/js/controllers/paths/createNewMultimediaDialogCtrl.js
@@ -1,13 +1,18 @@
 angular.module('consoleControllers.leg')
 
-.controller('CreateNewMultimediaElementDialogCtrl', function ($scope, addElementsFunction, saveFunction, 
-		dataService, leg) {
- 
+.controller('CreateNewMultimediaElementDialogCtrl', function ($scope, addElementsFunction, 
+		saveFunction, dataService, leg) {
+		$scope.errorMsg;
     $scope.newMedia = {type: 'image'};
-
+    $scope.loading = false;
+    
+    $scope.resetError = function() {
+    	$scope.errorMsg = undefined;
+    }
+    
     $scope.$modalSuccess = function() {
         if(!$scope.newMedia.link) {     // controlla che sia stato inserito un URL
-            alert("Non è stato inserito un indirizzo valido.");
+            $scope.errorMsg = "Non è stato inserito un indirizzo valido.";
         }
         else {
             if ($scope.newMedia.type) {
@@ -16,15 +21,16 @@ angular.module('consoleControllers.leg')
                 saveFunction();
             }
             else {
-                alert("Errore: il tipo dell'oggetto non è un tipo valido.");
+            	$scope.errorMsg = "Errore: il tipo dell'oggetto non è un tipo valido.";
             }
         }
     }
     
     $scope.uploadFile = function () {
+    	$scope.loading = true;
     	var fileInput = document.getElementById('upload-content-file');
     	if(fileInput.files.length == 0) {
-    		alert('Scegliere un file da caricare');
+    		$scope.errorMsg = "Scegliere un file da caricare";
     		return;
     	}
     	var file = fileInput.files[0];
@@ -37,9 +43,15 @@ angular.module('consoleControllers.leg')
     			"legId": leg.objectId,
           "formdata": formData,
     	};
-    	dataService.uploadFileContent(element).then(function (response) {
+    	dataService.uploadFileContent(element).then(
+    			function (response) {
             $scope.newMedia.link = response.data.link;
             $scope.file = null;
-        });
+            $scope.loading = false;
+    			},
+    			function (error) {
+    				$scope.errorMsg = "Errore di comunicazione con il sistema";
+    				$scope.loading = false;
+    			});
     };
 });
