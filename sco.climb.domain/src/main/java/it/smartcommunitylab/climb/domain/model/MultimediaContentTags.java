778154c903944886049dabf03de4ce75d997a308
micnori <micnori@yahoo.com>
2019-08-13 14:35:48 +0200

    new default tags (remove the existing document in multimediaContentTags)

--- sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/model/MultimediaContentTags.java
+++ sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/model/MultimediaContentTags.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 
 public class MultimediaContentTags {
 	@Id
 	private String id;
-	private List<String> tags = new ArrayList<>();
+	private List<String> subjects = new ArrayList<>();
+	private List<String> schoolYears = new ArrayList<>();
+	@Transient
+	private List<String> classes = new ArrayList<>();
 	
 	public String getId() {
 		return id;
@@ -16,10 +20,23 @@ public class MultimediaContentTags {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public List<String> getTags() {
-		return tags;
+	public List<String> getSubjects() {
+		return subjects;
 	}
-	public void setTags(List<String> tags) {
-		this.tags = tags;
+	public void setSubjects(List<String> subjects) {
+		this.subjects = subjects;
 	}
+	public List<String> getSchoolYears() {
+		return schoolYears;
+	}
+	public void setSchoolYears(List<String> schoolYears) {
+		this.schoolYears = schoolYears;
+	}
+	public List<String> getClasses() {
+		return classes;
+	}
+	public void setClasses(List<String> classes) {
+		this.classes = classes;
+	}
+	
 }
