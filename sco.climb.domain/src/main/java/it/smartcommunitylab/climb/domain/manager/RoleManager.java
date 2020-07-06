620563b0fb3dade2ca3fe13ef8a56a33f5bd64db
micnori <micnori@yahoo.com>
2020-07-06 16:46:01 +0200

    fix some roles

--- sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/manager/RoleManager.java
+++ sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/manager/RoleManager.java
@@ -127,14 +127,8 @@ public class RoleManager {
 		auth.setGameId(pedibusGame.getObjectId());
 		auth.getResources().add(Const.AUTH_RES_Institute);
 		auth.getResources().add(Const.AUTH_RES_School);
-		auth.getResources().add(Const.AUTH_RES_Child);
 		auth.getResources().add(Const.AUTH_RES_Image);
-		auth.getResources().add(Const.AUTH_RES_Volunteer);
-		auth.getResources().add(Const.AUTH_RES_Stop);
-		auth.getResources().add(Const.AUTH_RES_Route);
 		auth.getResources().add(Const.AUTH_RES_PedibusGame);
-		auth.getResources().add(Const.AUTH_RES_PedibusGame_Calendar);
-		auth.getResources().add(Const.AUTH_RES_PedibusGame_Excursion);
 		auths.add(auth);
 		
 		auth = new Authorization();
@@ -148,21 +142,22 @@ public class RoleManager {
 		auth.setSchoolId(school.getObjectId());
 		auth.setRouteId("*");
 		auth.setGameId(pedibusGame.getObjectId());
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Player);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Itinerary);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Leg);
 		auth.getResources().add(Const.AUTH_RES_PedibusGame_Link);
 		auths.add(auth);
 
 		auth = new Authorization();
-		auth.getActions().add(Const.AUTH_ACTION_READ);
-		auth.getActions().add(Const.AUTH_ACTION_ADD);
 		auth.getActions().add(Const.AUTH_ACTION_UPDATE);
-		auth.getActions().add(Const.AUTH_ACTION_DELETE);
 		auth.setRole(Const.ROLE_GAME_EDITOR);
 		auth.setOwnerId(ownerId);
 		auth.setInstituteId(institute.getObjectId());
 		auth.setSchoolId(school.getObjectId());
 		auth.setRouteId("*");
 		auth.setGameId("*");
-		auth.getResources().add(Const.AUTH_RES_Player);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Mobility);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Tuning);
 		auths.add(auth);
 		
 		storage.addUserRole(email, 
@@ -203,9 +198,29 @@ public class RoleManager {
 		auth.setSchoolId(school.getObjectId());
 		auth.setRouteId("*");
 		auth.setGameId(pedibusGame.getObjectId());
+		auth.getResources().add(Const.AUTH_RES_PedibusGame);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Mobility);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Itinerary);
 		auth.getResources().add(Const.AUTH_RES_PedibusGame_Calendar);
 		auth.getResources().add(Const.AUTH_RES_PedibusGame_Excursion);
 		auths.add(auth);
+		
+		auth = new Authorization();
+		auth.getActions().add(Const.AUTH_ACTION_READ);
+		auth.getActions().add(Const.AUTH_ACTION_UPDATE);
+		auth.getActions().add(Const.AUTH_ACTION_ADD);
+		auth.getActions().add(Const.AUTH_ACTION_DELETE);
+		auth.setRole(Const.ROLE_TEACHER);
+		auth.setOwnerId(ownerId);
+		auth.setInstituteId(institute.getObjectId());
+		auth.setSchoolId(school.getObjectId());
+		auth.setRouteId("*");
+		auth.setGameId(pedibusGame.getObjectId());
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Player);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Leg);
+		auth.getResources().add(Const.AUTH_RES_PedibusGame_Link);
+		auths.add(auth);
+		
 
 		storage.addUserRole(email, 
 				Utils.getAuthKey(ownerId, Const.ROLE_TEACHER, institute.getObjectId(), school.getObjectId(), 
