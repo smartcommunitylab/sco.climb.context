049659cf510f4f9c5a5bd43a020778019f0a9fbe
Matteo Chini <matteo.chini@gmail.com>
2021-08-05 17:08:17 +0200

    working with aac

---
+++ sco.climb.creation-dashboard/creation-dashboard/src/aac.js
@@ -0,0 +1,5 @@
+
+export const aacUrl="https://am-test.smartcommunitylab.it/aac"
+export const aacProfileUrl="https://am-test.smartcommunitylab.it/aac/"
+export const clientId="202e666e-ee54-4143-b92b-e4b65ab00d1d"
+export const clientSecret="05d5bc23-5feb-44d1-8845-0c3e3e6e3437"
