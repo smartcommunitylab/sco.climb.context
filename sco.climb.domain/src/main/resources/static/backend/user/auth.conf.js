e49e2b3a04be17a2f93b9072263d67dc5a73bc77
Matteo Chini <matteo.chini@gmail.com>
2022-11-23 16:59:00 +0100

    fixed v3 in redirect

--- sco.climb.domain/src/main/resources/static/backend/user/auth.conf.js
+++ sco.climb.domain/src/main/resources/static/backend/user/auth.conf.js
@@ -1,9 +1,9 @@
  var auth_conf = {
     authority: "https://aac.platform.smartcommunitylab.it/",
     client_id: "c_104137f7-0049-40ae-811e-ad33eb59fd36",
-    redirect_uri: window.location.protocol + "//" + window.location.host + "/domain/backend/user/callback.html",
-    post_logout_redirect_uri: window.location.protocol + "//" + window.location.host + "/domain/backend/user/index.html",
-    silent_redirect_uri: window.location.protocol + "//" + window.location.host + "/domain/backend/user/silent.html",
+    redirect_uri: window.location.protocol + "//" + window.location.host + "/v3/domain/backend/user/callback.html",
+    post_logout_redirect_uri: window.location.protocol + "//" + window.location.host + "/v3/domain/backend/user/index.html",
+    silent_redirect_uri: window.location.protocol + "//" + window.location.host + "/v3/domain/backend/user/silent.html",
     response_type: "token id_token",
     scope: "openid email profile",
     automaticSilentRenew: true,
