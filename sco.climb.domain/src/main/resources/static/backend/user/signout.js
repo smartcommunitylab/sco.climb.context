df7df9551bd1d38935ef3eca3133f3ad720999e7
Matteo Chini <matteo.chini@gmail.com>
2023-08-02 10:14:38 +0200

    fixed logout aac

--- sco.climb.domain/src/main/resources/static/backend/user/signout.js
+++ sco.climb.domain/src/main/resources/static/backend/user/signout.js
@@ -1,2 +1,5 @@
 
 var mgr = new Oidc.UserManager(auth_conf);
+localStorage.clear();
+sessionStorage.clear()
+window.location = window.location.protocol + "//" + window.location.host + "/domain/backend/user/index.html"
