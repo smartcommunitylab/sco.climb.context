43bbb080d625d78d89939f1654c604d4d76b60a5
micnori <micnori@users.noreply.github.com>
2023-09-15 14:47:20 +0200

    console data  with 403

--- sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/config/SecurityConfig.java
+++ sco.climb.domain/src/main/java/it/smartcommunitylab/climb/domain/config/SecurityConfig.java
@@ -65,7 +65,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                       oauth2.opaqueToken().introspector(tokenIntrospector());
                   }
               })
-              .anonymous().disable()
               // disable request cache, we override redirects but still better enforce it
               .requestCache((requestCache) -> requestCache.disable())
               .exceptionHandling()
