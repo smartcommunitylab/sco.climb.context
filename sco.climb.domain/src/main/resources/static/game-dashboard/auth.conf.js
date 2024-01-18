aa7c1ec2d1401881fb256db97c9df59a5e104271
Matteo Chini <matteo.chini@gmail.com>
2024-01-18 15:01:37 +0100

    new clientd id

--- sco.climb.domain/src/main/resources/static/game-dashboard/auth.conf.js
+++ sco.climb.domain/src/main/resources/static/game-dashboard/auth.conf.js
@@ -1,9 +1,9 @@
  var auth_conf = {
     authority: "https://aac.platform.smartcommunitylab.it/",
-    client_id: "c_104137f7-0049-40ae-811e-ad33eb59fd36",
-    redirect_uri:  window.location.protocol + "//" + window.location.host + "/v3/game-dashboard/callback.html",
-    post_logout_redirect_uri: window.location.protocol + "//" + window.location.host + "/v3/game-dashboard/signout.html",
-    silent_redirect_uri: window.location.protocol + "//" + window.location.host + "/v3/game-dashboard/silent.html",
+    client_id: "c_fc44aa82a6b04c2099b8d81058b47ee6",
+    redirect_uri:  window.location.protocol + "//" + window.location.host + "/domain/game-dashboard/callback.html",
+    post_logout_redirect_uri: window.location.protocol + "//" + window.location.host + "/domain/game-dashboard/signout.html",
+    silent_redirect_uri: window.location.protocol + "//" + window.location.host + "/domain/game-dashboard/silent.html",
     response_type: "token id_token",
     scope: "openid email profile",
     automaticSilentRenew: true,
